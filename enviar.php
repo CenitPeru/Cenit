e3a5c73e17ccae0b16d7a9b18231ecbf9df5e307
CenitPeru <70306170+CenitPeru@users.noreply.github.com>
2020-08-27 10:49:23 -0500

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,11 +1,14 @@
 <?php
-$name= $_POST['nombre'];
-$lastname= $_POST['apellido'];
+if(isset($_POST["nombre"])&&! empty($_POST["nombre"])&&
+isset($_POST["apellido"])&&! empty($_POST["apellido"])&&
+isset($_POST["email"])&&! empty($_POST["email"])&&
+isset($_POST["texto"])&&! empty($_POST["texto"]))
+$name= $_POST['nombre']."".$_POST['apellido'];
 $mail= $_POST['email'];
 $text= $_POST['texto'];
 
 $header = 'From: ' . $mail .  "\r\n";
-$message = "Este mensaje fue enviado por: " . $name . $lastname . "\r\n";
+$message = "Este mensaje fue enviado por: " . $name . "\r\n";
 $message = "Su email es: " . $mail . "\r\n";
 $message = "El mensaje es: " . $text . "\r\n";
 
@@ -13,5 +16,5 @@ $para= 'kendysatomicos@hotmail.com'
 $asunto = 'Asunto del mensaje'
 
 mail($para,$asunto,utf8_decode($message),$header);
-header("Location:https://cenitperu.github.io/Cenit/");
+header("Location:index.html");
 ?>
