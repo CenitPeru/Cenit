c9b816e727923f8555a68c55affa84d0abb7bdf8
CenitPeru <70306170+CenitPeru@users.noreply.github.com>
2020-08-27 12:40:10 -0500

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -1,20 +1,4 @@
 <?php
-if(isset($_POST["nombre"])&&! empty($_POST["nombre"])&&
-isset($_POST["apellido"])&&! empty($_POST["apellido"])&&
-isset($_POST["email"])&&! empty($_POST["email"])&&
-isset($_POST["texto"])&&! empty($_POST["texto"]))
-$name= $_POST['nombre']."".$_POST['apellido'];
-$mail= $_POST['email'];
-$text= $_POST['texto'];
 
-$header = 'From: ' . $mail .  "\r\n";
-$message = "Este mensaje fue enviado por: " . $name . "\r\n";
-$message = "Su email es: " . $mail . "\r\n";
-$message = "El mensaje es: " . $text . "\r\n";
-
-$para= 'kendysatomicos@hotmail.com'
-$asunto = 'Asunto del mensaje'
-
-mail($para,$asunto,utf8_decode($message),$header);
-header("Location:index.html");
+echo "las variables llegan bien";
 ?>
